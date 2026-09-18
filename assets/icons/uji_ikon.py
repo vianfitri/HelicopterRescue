b6573586aedefa28ff5c31d8db4340bc144a7c38
vianfitri <viancukiep@gmail.com>
2026-09-18 10:44:30 +0700

    commit

--- assets/icons/uji_ikon.py
+++ assets/icons/uji_ikon.py
@@ -13,10 +13,10 @@ class IconTestPanel(wx.Panel):
         
         if gc:
             # Uji menggambar dalam 3 ukuran berbeda
-            #IconRenderer.draw_settings(gc, x=30, y=30, size=32, color=wx.Colour(255, 255, 255))
-            #IconRenderer.draw_settings(gc, x=100, y=30, size=64, color=wx.Colour(0, 200, 255))
-            #IconRenderer.draw_settings(gc, x=200, y=30, size=128, color=wx.Colour(255, 180, 0))
-            IconRenderer.draw_plug(gc, x=30, y=30, size=64, color=wx.Colour(0, 255, 0))
+            IconRenderer.draw_settings(gc, x=30, y=30, size=32, color=wx.Colour(255, 255, 255))
+            IconRenderer.draw_settings(gc, x=100, y=30, size=64, color=wx.Colour(0, 200, 255))
+            IconRenderer.draw_settings(gc, x=200, y=30, size=128, color=wx.Colour(255, 180, 0))
+            IconRenderer.draw_plug(gc, x=300, y=30, size=64, color=wx.Colour(0, 255, 0))
 
             #IconRenderer.draw_plug(gc, x=30, y=90, size=64, color=wx.Colour(255, 0, 0), connected=False)
 
