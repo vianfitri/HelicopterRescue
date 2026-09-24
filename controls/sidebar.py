283f166aac9e0faa68febee9cacc500fe36cdeeb
vianfitri <viancukiep@gmail.com>
2026-09-24 16:31:36 +0700

    commit

--- controls/sidebar.py
+++ controls/sidebar.py
@@ -50,7 +50,8 @@ class SidebarControl(wx.Panel):
 
     def _on_paint(self, event):
         dc = wx.AutoBufferedPaintDC(self)
-        dc.SetBackground(wx.Brush(wx.Colour(255, 0, 0)))
+        dc.SetBackground(wx.Brush(wx.Colour(8, 15, 25)))
+        #dc.SetBackground(wx.Brush(wx.Colour(255, 0, 0)))
         dc.Clear()
 
         # Right border line separating sidebar from content
